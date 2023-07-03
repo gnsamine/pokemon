3796c2e8e5413feacc1228d10cd76391232b5d88
gnsamine <gunesamine0@gmail.com>
2023-07-03 19:12:55 +0300

    names

--- pokemon.go
+++ pokemon.go
@@ -1,1 +1,32 @@
-package pokemon
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Name struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	resp, err := http.Get("https://pokeapi.co/api/v2/type/ghost/")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	sb := string(body)
+	log.Printf(sb)
+
+}
+
+type PokemonSpecies struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
