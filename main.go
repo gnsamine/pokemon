d99d30ecfdacf9cf5c8931033d35e620c6ad7542
gnsamine <gunesamine0@gmail.com>
2023-07-04 18:31:36 +0300

    getnamesfinal

--- main.go
+++ main.go
@@ -5,22 +5,6 @@ import (
 )
 
 func main() {
-	// resp, err := http.Get("https://pokeapi.co/api/v2/type/ghost/")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// body, err := io.ReadAll(resp.Body)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// stringBody := string(body)
-	// fmt.Println(stringBody)
-
-	// we use "var data map[string]interface{}"
-	//bc values can be of any type with interface{}
-
-	//fmt.Println(poke.SampleData)
-
 	poke.GetNames()
 
 }
