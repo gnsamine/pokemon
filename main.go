258faeebd2976a5bf6fbcf1427fa40fe6bda7348
gnsamine <gunesamine0@gmail.com>
2023-07-05 10:46:18 +0300

    getgo

--- main.go
+++ main.go
@@ -5,6 +5,6 @@ import (
 )
 
 func main() {
-	poke.GetNames()
+	poke.GetSprites()
 
 }
--- poke/pokemonType.go
+++ poke/pokemonType.go
@@ -3,6 +3,8 @@ package poke
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 )
 
 type Pokemon struct {
@@ -14,35 +16,51 @@ type Pokemon struct {
 	} `json:"pokemon"`
 
 	Sprites struct {
-		FrontDefault string `json:"front_default"`
+		BackDefault      string      `json:"back_default"`
+		BackFemale       interface{} `json:"back_female"`
+		BackShiny        string      `json:"back_shiny"`
+		BackShinyFemale  interface{} `json:"back_shiny_female"`
+		FrontDefault     string      `json:"front_default"`
+		FrontFemale      interface{} `json:"front_female"`
+		FrontShiny       string      `json:"front_shiny"`
+		FrontShinyFemale interface{} `json:"front_shiny_female"`
 	} `json:"sprites"`
 }
 
-func GetNames() {
-	jsonData := SampleData
+//func GetNames() {
+// jsonData := SampleData
+
+// var p Pokemon
+// json.Unmarshal([]byte(jsonData), &p)
+
+// fmt.Println("Names of Ghost Pokemons")
+// for i := 0; i < len(p.Pokemon); i++ {
+// 	fmt.Println(p.Pokemon[i].Pokemon.Name)
+// }
+//}
+
+func GetSprites() {
+	resp, err := http.Get("https://pokeapi.co/api/v2/type/ghost/")
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	stringBody := string(body)
+	//fmt.Println(stringBody)
 
 	var p Pokemon
-	json.Unmarshal([]byte(jsonData), &p)
+	json.Unmarshal([]byte(stringBody), &p)
 
 	fmt.Println("Names of Ghost Pokemons")
 	for i := 0; i < len(p.Pokemon); i++ {
 		fmt.Println(p.Pokemon[i].Pokemon.Name)
+
+		// 	// we use "var data map[string]interface{}"
+		// 	//bc values can be of any type with interface{}
+
 	}
+
 }
-
-// func GetSprites() {
-// 	resp, err := http.Get("https://pokeapi.co/api/v2/type/ghost/")
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	body, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	stringBody := string(body)
-// 	fmt.Println(stringBody)
-
-// 	// we use "var data map[string]interface{}"
-// 	//bc values can be of any type with interface{}
-
-// }
